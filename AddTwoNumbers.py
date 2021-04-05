22239ce41920ddfd2d159076a625acc9faad80a3
greenbeam <oreshka@gmail.com>
2021-04-05 18:23:34 +0300

    Add Two Numbers

---
+++ AddTwoNumbers.py
@@ -0,0 +1,52 @@
+"""
+Add Two Numbers
+Medium
+
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order, and each of their nodes contains a single digit.
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0
+        result = ListNode(0)
+        lr = result
+        while l1 or l2 or carry > 0:
+            num_one, num_two = 0, 0
+            if l1:
+                num_one = l1.val
+                l1 = l1.next
+            if l2:
+                num_two = l2.val
+                l2 = l2.next
+            val = num_one + num_two + carry
+            carry = 0
+            if val >= 10:
+                val = val - 10
+                carry = 1
+            lr.val = val
+            if l2 or l1 or carry > 0:
+                lr.next = ListNode(0)
+                lr = lr.next
+        return result
